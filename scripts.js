76c2aa634874f6fc93d2f43c627ca30b765345ee
svvdevell <svvdevel@gmail.com>
2022-08-17 22:39:15 +0300

    added second task

--- scripts.js
+++ scripts.js
@@ -2,9 +2,20 @@
 
 // 1 
 
-function sum(x) {
-    if (x == 1 ) return x;
-    return x + sum(x - 1);
-}
+// function sum(x) {
+//     if (x == 1 ) return x;
+//     return x + sum(x - 1);
+// }
 
-console.log(sum(4));
+// console.log(sum(4));
+
+// 2 
+
+function findSmallWords(str) {
+    let words = str.split(' ');
+    let result = words.reduce((shortestWord, currentWord) => {
+        return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+    }, words[0]);
+    return result;
+}
+console.log(findSmallWords("Lorem, ipsum dolor sit amet consectetur adipisicing elit."));
